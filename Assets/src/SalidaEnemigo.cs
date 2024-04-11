e9708d05f848fb35e4e6f556ede7bd0b29c8eaf3
Tosira <toskareymun@gmail.com>
2024-04-11 11:54:59 -0400

    initial commit

---
+++ Assets/src/SalidaEnemigo.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SalidaEnemigo : MonoBehaviour
+{
+    [SerializeField] private GameObject prefabEnemigo;
+    [SerializeField] private Transform target;     
+    private float[] tiempos = new float[] { 3, 5, 7 };
+    private float tiempo = -1;
+
+    // Update is called once per frame
+    void Update()
+    {        
+        if (tiempo == -1)
+        {
+            int indiceTiempo = Random.Range(0, 3);
+            tiempo = tiempos[indiceTiempo];
+            Debug.Log("indice: " + indiceTiempo + ", timepo: " + tiempo);  
+        } 
+        else
+        {
+            tiempo -= Time.deltaTime;  
+            if (tiempo <= 0)
+            {
+                GameObject gameObjectEnemigo = GameObject.Instantiate(prefabEnemigo);
+                gameObjectEnemigo.transform.position = this.transform.position; 
+                Enemigo enemigo = gameObjectEnemigo.GetComponent<Enemigo>();
+                enemigo.target = target;
+                tiempo = -1; 
+            }
+        }            
+    }
+}
