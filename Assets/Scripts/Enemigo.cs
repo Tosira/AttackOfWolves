6b46253d1fce6b1fb51f6d50e01faf1376d81939
xWTomasWx <117607372+xWTomasWx@users.noreply.github.com>
2024-04-19 16:40:42 -0400

    Arte para beta

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; 
+
+public class Enemigo : MonoBehaviour
+{
+    public Transform target; 
+    NavMeshAgent agent;    
+
+    // duda con el agente que esta en privado y aun asi los enemigos se mueven. 
+    // implementar vida de los enemigos
+
+    public int vidaActual; 
+    public int vidaMaxima; 
+    public Vector3 velocidad;
+
+    public BarraDeVida barraV;
+
+    private Animator animator;
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    public void SetAgent()
+    {
+        //Debug.Log("Agente Seteado");
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    public virtual void moverAgenteNavMesh()
+    {
+        agent.SetDestination(target.position);
+    }
+
+    public virtual void Atacar() { }
+
+    public void RecibirAtaque(int damage)
+    {
+        if ((vidaActual -= damage) <= 0)
+        {
+            Destroy(gameObject); 
+        }
+        barraV.actualizarBarraVida(vidaMaxima, vidaActual);
+    }
+}
